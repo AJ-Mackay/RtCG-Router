3b8b8d2248b6c364a94b108aeb0b15fcb4c16749
Paul Mackay <paul.m.mackay85@gmail.com>
2021-11-03 14:26:52 +0000

    Practice Redirecting & Extracting Params

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,15 @@
+import { Fragment } from 'react';
+import { useParams } from 'react-router-dom';
+
 const QuoteDetail = () => {
-  return <h1>Quote Detail Page</h1>
+  const params = useParams();
+
+  return (
+    <Fragment>
+      <h1>Quote Detail Page</h1>
+      {params.quoteId}
+    </Fragment>
+  );
 };
 
 export default QuoteDetail;
